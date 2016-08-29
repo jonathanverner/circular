7ab1e7c5a17ef3b3272341dd75c29d499e7649f0
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2016-08-30 01:32:29 +0200

    FEATURE: Add a Model plugin.

--- src/circular/template/tag.py
+++ src/circular/template/tag.py
@@ -603,3 +603,86 @@ class For(TagPlugin):
         ret += ">"
         return ret
 TplNode.register_plugin(For)
+
+class Model(TagPlugin):
+    """
+        The model plugin binds an input element value to a context variable (or a simple expression).
+        For example the template:
+        ```
+            Hello {{ name }}
+            <input model='name' type='textinput' />
+        ```
+        when bound to a context would update the value of the variable `name` in the context
+        whenever a user typed (or pasted in) a new value on the input field. The plugin takes
+        two optional parameters: `update_event` and `update_interval`. When `update_event` is
+        set, the context is updated whenever the given event is fired on the input element.
+        The default is to update whenever the event `input` is fired. When `update_interval`
+        is set to number, the context is updated at most once per the given number of milliseconds.
+
+        WARNING: The Model plugin must come before any plugin which creates more than one
+        element (e.g. the `For` plugin).
+    """
+
+
+    def __init__(self, tpl_element,model=None,update_interval=None,update_event='input'):
+        super().__init__(tpl_element)
+        self._model_change_timer = None
+        self._input_change_timer = None
+        if isinstance(tpl_element,Model):
+            self._update_event = tpl_element._update_event
+            self._update_interval = tpl_element._update_interval
+            self._model_observer = tpl_element.model.clone()
+            self.child = tpl_element.child.clone()
+        else:
+            self._update_event = update_event
+            if update_interval is not None:
+                self._update_interval = int(update_interval)
+            else:
+                self._update_interval = None
+            self._model_observer = ExpObserver(model)
+            self.child = TplNode(tpl_element)
+        if self._update_interval:
+            self._model_observer.bind('change',self._defer_model_change)
+        else:
+            self._model_observer.bind('change',self._model_change)
+        self.child.bind('change',self._subtree_change_handler)
+        logger.debug("Initialized model plugin")
+
+    def bind_ctx(self,ctx):
+        self.element = self.child.bind_ctx(ctx)
+        self._model_observer.context = ctx
+        if self._update_interval:
+            self.element.bind(self._update_event,self._defer_input_change)
+        else:
+            self.element.bind(self._update_event,self._input_change)
+        super().bind_ctx(ctx)
+        return self.element
+
+    def _defer_model_change(self,event):
+        if self._model_change_timer is None:
+            self._model_change_timer = timer.set_interval(self._model_change,self._update_interval)
+
+    def _defer_input_change(self,event):
+        if self._input_change_timer is None:
+            self._input_change_timer = timer.set_interval(self._input_change,self._update_interval)
+
+    def _model_change(self,event=None):
+        if self.element and self._model_observer.have_value() and self.element.value != self._model_observer.value:
+            self.element.value = self._model_observer.value
+            if self._model_change_timer:
+                timer.clear_interval(self._model_change_timer)
+                self._model_change_timer = None
+
+    def _input_change(self,event=None):
+        if self.element and (not self._model_observer.have_value() or self.element.value != self._model_observer.value):
+            self._model_observer.evaluate_assignment(self.element.value)
+            if self._input_change_timer:
+                timer.clear_interval(self._input_change_timer)
+                self._input_change_timer = None
+
+    def __repr__(self):
+        if self._model_observer.have_value():
+            return "<ModelPlugin "+self._model_observer._exp_src+" ("+self._model_observer.value+")>"
+        else:
+            return "<ModelPlugin "+self._model_observer._exp_src+" (undefined)>"
+TplNode.register_plugin(Model)
--- tests/circular/template/test_tag.py
+++ tests/circular/template/test_tag.py
@@ -244,4 +244,29 @@ def test_nested_for():
     assert len(filter_comments(nocomment_children[0].children)) == 2
 
 
+def test_model_plugin():
+    div_elem = MockElement('div',id='test')
+    text_elem = MockElement('#text',id='text')
+    text_elem.text = "{{ c['name'] }}"
+    div_elem <= text_elem
+    input_elem = MockElement('input',model='c["name"]',id='input')
+    div_elem <= input_elem
+    document <= div_elem
+    tpl = Template(document['test'])
+    ctx = Context({})
+    ctx.c = {'name':''}
+    elem=tpl.bind_ctx(ctx)
+    text_elem = document['text']
+    input_elem = document['input']
+    assert text_elem.text == ''
+    input_elem.setAttribute('value','Jonathan')
+    assert text_elem.text == 'Jonathan'
+    assert ctx.c['name'] == 'Jonathan'
+    assert input_elem.value == 'Jonathan'
+    input_elem.value = 'Test'
+    assert text_elem.text == 'Jonathan'
+    assert ctx.c['name'] == 'Jonathan'
+    ctx.c['name'] = 'Test2'
+    assert input_elem.value == 'Test2'
+    assert text_elem.text == 'Test2'
 
