071dad2e108b6b2456cf6d40c52abcb7748e3e57
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2016-09-07 21:43:52 +0200

    Don't suppress test-server output (for debugging purposes)

--- management/test/run_tests.sh
+++ management/test/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
-python -m SimpleHTTPServer 7000 2>/dev/null >/dev/null &
+python -m SimpleHTTPServer 7000 &
 
 PYTHONPATH='./src/:./tests/brython/' python -m pytest -rw --driver PhantomJS --ignore=./tests/selenium/webroot/ --ignore=./tests/results/tests tests
