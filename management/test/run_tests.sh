d113842bc892732c435e034fd6d8fa53ce4ec3b5
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2016-09-07 21:51:30 +0200

    BUGFIX: TravisCI has python3: SimpleHTTPServer => http.server.

--- management/test/run_tests.sh
+++ management/test/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
-python -m SimpleHTTPServer 7000 &
+python -m http.server 7000 &
 
 PYTHONPATH='./src/:./tests/brython/' python -m pytest -rw --driver PhantomJS --ignore=./tests/selenium/webroot/ --ignore=./tests/results/tests tests
