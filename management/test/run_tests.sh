55b63f1c722976786768da5a44afc47f2af553b3
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2016-09-07 21:38:39 +0200

    BUGFIX: Specify test directory for Travis-CI run_test script.

--- management/test/run_tests.sh
+++ management/test/run_tests.sh
@@ -2,4 +2,4 @@
 set -ev
 python -m SimpleHTTPServer 7000 2>/dev/null >/dev/null &
 
-PYTHONPATH='./src/:./tests/brython/' python -m pytest -rw --driver PhantomJS --ignore=./tests/selenium/webroot/ --ignore=./tests/results/tests
+PYTHONPATH='./src/:./tests/brython/' python -m pytest -rw --driver PhantomJS --ignore=./tests/selenium/webroot/ --ignore=./tests/results/tests tests
