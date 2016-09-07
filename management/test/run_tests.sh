eb2d707da792e2881afca3cb5e2aed2d43639664
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2016-09-07 22:19:16 +0200

    BUGFIX: Start the HTTP Server in the correct directory (and cache pip).

--- management/test/run_tests.sh
+++ management/test/run_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ev
+cd ./tests/selenium/webroot
 python -m http.server 7000 &
+cd ../../../
 
 PYTHONPATH='./src/:./tests/brython/' python -m pytest -rw --driver PhantomJS --ignore=./tests/selenium/webroot/ --ignore=./tests/results/tests tests
